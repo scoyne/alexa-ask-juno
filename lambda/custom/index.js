cd65225b0ecea260aff602dc12c14bdd1f36e8a1
scoyne <scoyne@bw.edu>
2018-10-23 20:46:42 -0400

    updating index.md

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,150 +1,67 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
 
-const Alexa = require('ask-sdk-core');
-const questions = require('./questions');
-const i18n = require('i18next');
-const sprintf = require('i18next-sprintf-postprocessor');
+const Alexa = require('ask-sdk');
 
-/* INTENT HANDLERS */
-const LaunchRequestHandler = {
+const GetNewFactHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest'
+      || (request.type === 'IntentRequest'
+        && request.intent.name === 'GetNewFactIntent');
   },
   handle(handlerInput) {
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-
-    const item = requestAttributes.t(getRandomItem(Object.keys(questions.QUESTION_EN_US)));
-
-    const speakOutput = requestAttributes.t('WELCOME_MESSAGE', requestAttributes.t('SKILL_NAME'), item);
-    const repromptOutput = requestAttributes.t('WELCOME_REPROMPT');
-
-    handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+    const factArr = data;
+    const factIndex = Math.floor(Math.random() * factArr.length);
+    const randomFact = factArr[factIndex];
+    const speechOutput = GET_FACT_MESSAGE + randomFact;
 
     return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .reprompt(repromptOutput)
+      .speak(speechOutput)
+      .withSimpleCard(SKILL_NAME, randomFact)
       .getResponse();
   },
-};
-
-const QuestionHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'QuestionIntent';
-  },
-  handle(handlerInput) {
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-
-    const itemSlot = handlerInput.requestEnvelope.request.intent.slots.Item;
-    let itemName;
-    if (itemSlot && itemSlot.value) {
-      itemName = itemSlot.value.toLowerCase();
-    }
-
-    const cardTitle = requestAttributes.t('DISPLAY_CARD_TITLE', requestAttributes.t('SKILL_NAME'), itemName);
-    const myQuestions = requestAttributes.t('QUESTIONS');
-    const question = myQuestions[itemName];
-    let speakOutput = "";
-
-    if (question) {
-      sessionAttributes.speakOutput = question;
-      //sessionAttributes.repromptSpeech = requestAttributes.t('QUESTION_REPEAT_MESSAGE');
-      handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-
-      return handlerInput.responseBuilder
-        .speak(sessionAttributes.speakOutput) // .reprompt(sessionAttributes.repromptSpeech)
-        .withSimpleCard(cardTitle, question)
-        .getResponse();
-    }
-    else{
-      speakOutput = requestAttributes.t('QUESTION_NOT_FOUND_MESSAGE');
-      const repromptSpeech = requestAttributes.t('QUESTION_NOT_FOUND_REPROMPT');
-      if (itemName) {
-        speakOutput += requestAttributes.t('QUESTION_NOT_FOUND_WITH_ITEM_NAME', itemName);
-      } else {
-        speakOutput += requestAttributes.t('QUESTION_NOT_FOUND_WITHOUT_ITEM_NAME');
-      }
-      speakOutput += repromptSpeech;
-
-      sessionAttributes.speakOutput = speakOutput; //saving speakOutput to attributes, so we can use it to repeat
-      sessionAttributes.repromptSpeech = repromptSpeech;
-
-      handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-
-      return handlerInput.responseBuilder
-        .speak(sessionAttributes.speakOutput)
-        .reprompt(sessionAttributes.repromptSpeech)
-        .getResponse();
-    }
-  }
 };
 
 const HelpHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-
-    const item = requestAttributes.t(getRandomItem(Object.keys(questions.QUESTION_EN_US)));
-
-    sessionAttributes.speakOutput = requestAttributes.t('HELP_MESSAGE', item);
-    sessionAttributes.repromptSpeech = requestAttributes.t('HELP_REPROMPT', item);
-
     return handlerInput.responseBuilder
-      .speak(sessionAttributes.speakOutput)
-      .reprompt(sessionAttributes.repromptSpeech)
-      .getResponse();
-  },
-};
-
-const RepeatHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent';
-  },
-  handle(handlerInput) {
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-
-    return handlerInput.responseBuilder
-      .speak(sessionAttributes.speakOutput)
-      .reprompt(sessionAttributes.repromptSpeech)
+      .speak(HELP_MESSAGE)
+      .reprompt(HELP_REPROMPT)
       .getResponse();
   },
 };
 
 const ExitHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent'
-                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent');
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
   },
   handle(handlerInput) {
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    const speakOutput = requestAttributes.t('STOP_MESSAGE', requestAttributes.t('SKILL_NAME'));
-
     return handlerInput.responseBuilder
-      .speak(speakOutput)
+      .speak(STOP_MESSAGE)
       .getResponse();
   },
 };
 
 const SessionEndedRequestHandler = {
   canHandle(handlerInput) {
-    console.log("Inside SessionEndedRequestHandler");
-    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
   },
   handle(handlerInput) {
-    console.log(`Session ended with reason: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
     return handlerInput.responseBuilder.getResponse();
   },
 };
-
 
 const ErrorHandler = {
   canHandle() {
@@ -154,75 +71,54 @@ const ErrorHandler = {
     console.log(`Error handled: ${error.message}`);
 
     return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .speak('Sorry, an error occurred.')
+      .reprompt('Sorry, an error occurred.')
       .getResponse();
   },
 };
 
-/* CONSTANTS */
-const skillBuilder = Alexa.SkillBuilders.custom();
-const languageStrings = {
-  en: {
-    translation: {
-      QUESTIONS: questions.QUESTION_EN_US,
-      SKILL_NAME: 'Ask Juno',
-      WELCOME_MESSAGE: 'Welcome to %s. You can ask a question like, what was the name of the first %s? ... Now, what can I help you with?',
-      WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
-      DISPLAY_CARD_TITLE: '%s  - Question for %s.',
-      HELP_MESSAGE: 'You can ask questions such as, can I pet a %s, or, you can say exit...Now, what can I help you with?',
-      HELP_REPROMPT: 'You can say things like, is it safe to travel with a %s, or you can say exit...Now, what can I help you with?',
-      STOP_MESSAGE: 'Goodbye!',
-      QUESTION_REPEAT_MESSAGE: 'Try saying repeat.',
-      QUESTION_NOT_FOUND_MESSAGE: 'I\'m sorry, I currently do not know ',
-      QUESTION_NOT_FOUND_WITH_ITEM_NAME: 'the question for %s. ',
-      QUESTION_NOT_FOUND_WITHOUT_ITEM_NAME: 'that question. ',
-      QUESTION_NOT_FOUND_REPROMPT: 'What else can I help with?'
-    },
-  },
-  'en-US': {
-    translation: {
-      QUESTIONS: questions.QUESTION_EN_US,
-      SKILL_NAME: 'Ask Juno'
-    },
-  },
-};
+const SKILL_NAME = 'Space Facts';
+const GET_FACT_MESSAGE = 'Here\'s your fact: ';
+const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
 
-// Finding the locale of the user
-const LocalizationInterceptor = {
-  process(handlerInput) {
-    const localizationClient = i18n.use(sprintf).init({
-      lng: handlerInput.requestEnvelope.request.locale,
-      overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
-      resources: languageStrings,
-      returnObjects: true
-    });
+const data = [
+  'Many people with disabilities use a service animal in order to fully participate in everyday life. Dogs can be trained to perform many important tasks to assist people with disabilities, such as providing stability for a person who has difficulty walking, picking up items for a person who uses a wheelchair, preventing a child with autism from wandering away, or alerting a person who has hearing loss when someone is approaching from behind.',
+  'The Department of Justice continues to receive many questions about how the Americans with Disabilities Act (ADA) applies to service animals. The ADA requires State and local government agencies, businesses, and non-profit organizations (covered entities) that provide goods or services to the public to make "reasonable modifications" in their policies, practices, or procedures when necessary to accommodate people with disabilities. The service animal rules fall under this general principle. Accordingly, entities that have a "no pets" policy generally must modify the policy to allow service animals into their facilities. This publication provides guidance on the ADA\'s service animal provisions and should be read in conjunction with the publication ADA Revised Requirements: Service Animals.',
+  'Under the Americans with Disabilities Act, a service animal is defined as a dog that has been individually trained to do work or perform tasks for an individual with a disability.  The task(s) performed by the dog must be directly related to the person\'s disability.',
+  'The service animal must be trained to take a specific action when needed to assist the person with a disability. For example, a person with diabetes may have a service animal that is trained to alert a person when their blood sugar reaches high or low levels. A person with depression may have an animal that is trained to remind them to take their medication. Or, a person who has epilepsy may have a service animal that is trained to detect the onset of a seizure and then help the person remain safe during the seizure.',
+  'Emotional support, therapy, comfort, or companion animals are terms used to describe animals that provide comfort just by being with a person.  Because they have not been trained to perform a specific job or task, they do not qualify as service animals under the American with Disabilities Act.',
+  'Under the Americans with Disabilities Act, service animals must be trained before they can be taken into public places.  However, some State or local laws cover animals that are still in training..',
+  'In situations where it is not obvious that the dog is a service animal, business owners or employeers may ask only two specific questions: (1) is the dog a service animal required because of a disability? and (2) what work or task has the dog been trained to perform? business owners are not allowed to request any documentation for the dog, require that the dog demonstrate its task, or inquire about the nature of the person\'s disability..',
+  'A guide dog is trained to guide his client in a straight line unless told otherwise, avoiding obstacles on the ground, to both sides and above. The service animal will stop at curbs, stairs, and locate doorways to regularly visited destinations. The service animal should maintain a straight line when crossing a road, but will not decide when it is safe to cross. Making the judgment as to when it is safe to cross a road with a guide dog would be identical to making that judgment when using a white cane. It is the client’s responsibility to decide when and where to cross roads.',
+  'Working a guide dog is a partnership that offers a potentially unique, safe and effective way of getting around independently.',
+  'Thousands of visually impaired people in all walks of life have found that a service animal brings new freedom and independence. In a world where the pace of life and crowded noisy streets can make getting around difficult and stressful for a person with a visual impairment, a service animal can greatly increase your confidence. A true partnership can develop with the service animal becoming a companion and friend. In addition, many clients find that a service animal is a social asset. People often are interested and curious about service animals and often approach to chat or offer assistance. This can reduce the feeling of isolation which some visually impaired people experience.',
+  'Guide dogs are allowed everywhere that the general public is allowed to go.  This includes restaurants, taxi cabs, airplanes, hotels, etcetera.  This right is protected by a federal law called the Americans With Disabilities Act.',
+  'The general rule is that working guide dogs should be ignored. Distractions take their concentration away from the work they have to do which can put the dog and its teammate in jeopardy.  Do not pet or feed a guide dog and do not encourage the dog to misbehave.',
+  'A guide dog is trained to assist persons with visual impairments.  A service dog or service animal is trained to help those with severe to profound hearing loss by alerting the individual to a variety of sounds or someone with a physical impairment that substantially limits mobility  by assisting in the performance of a wide variety of tasks depending on need and training for example opening doors, retrieving, etcetera.',
+  'The average working life for a guide dog dog is 7 - 8 years. Many guide dogs have worked to the ages of 10 or 11.',
+  'Labrador Retrievers are currently the most popular breed used for Guide dogs. Most programs use Labrador Retrievers, Golden Retrievers, German Shepherds, or crosses between Golden Retrievers and Labrador Retrievers as guide dog candidates. Some programs use Labradoodles, Boxers, or Chesapeake Bay Retrievers. These breeds are chosen for their intelligence, ability and willingness to work long hours, ability to tolerate stress, good health, and public acceptability or recognition',
+  'A guide dog should have rock-solid nerves. The should be calm and confident, obeying even in the midst of chaos. He should not be easily frightened but also should not be ignorant of real danger when it presents itself. Just as you wouldn\'t want a guide dog who trembled at the sight of a passing car, you would not want one who stood happily in the road as one speed straight toward it. In other words, you want a dog with an abundance of good common sense.  A guide dog should be biddable, which means he should have a desire to please his master and to work as a team member, choosing to perform his job out of loyalty even when it is unpleasant and he\'d rather be doing something else (like staying home warm in bed instead of out on the streets in the sleet taking his master to the pharmacy for essential medication). The guide dog should be intelligent and trainable. He should be an excellent problem-solver because it is impossible to predict every possible puzzle a dog might encounter in his working life and he must be able to apply what he knows creatively in new situations to make safe and reasonable decisions. The ability to exhibit "intelligent disobedience" is also prized. A guide dog intelligently disobeys a command to go forward when it would put his master in danger, such as when a car is coming. When the dog refuses the command, it falls to the owner to determine why and then make an informed decision on whether to proceed anyway, wait, or take a different path.',
+  'In the year 79 excavations in Pompeii reveal a wall-painting of a blind man apparently being led by his dog.',
+  'Please don\'t touch, talk, feed or otherwise distract a service dog while they are wearing their harness or vest. You should allow the dog to concentrate and perform for the safety of his handler.',
+  'Don\'t treat the service dog as a pet, while they are wearing their harness or vest, give them the respect of a working dog.',
+  'Speak to the handler, not the dog. Some handlers will allow petting, but be sure to ask before doing so. If allowed, don\'t pat the dog on the head; stroke the dog on the shoulder area.',
+  'If the service dog handler says no when you ask to pet the dog, don\’t be offended. The dog or handler might be having a bad day, or he might be in a hurry. Remember, a service dog is as vital to a disabled person as a wheelchair or cane. You wouldn\'t ask to pet their wheelchair or get mad if they wouldn\'t let you pet their cane.',
+  'You should not give a service dog commands; allow the handler give the commands.',
+  'When walking with a guide or service dog team, you should not walk on the dog\'s left side, as it may become distracted or confused. Ask the handler where you should walk. Depending on the situation, they may ask you to walk ahead of them on their right side, or behind them by their right shoulder.',
+  'Never attempt to grab or steer the person while the dog is guiding or attempt to hold the dog\'s harness. You should ask if the handler needs your assistance and, if so, offer your left arm.',
+  'Never give a service animal table scraps. You should respect the handler\'s need to give the dog a balanced diet, and to maintain its good habits.'
+];
 
-    const attributes = handlerInput.attributesManager.getRequestAttributes();
-    attributes.t = function (...args) {
-      return localizationClient.t(...args);
-    };
-  },
-};
+const skillBuilder = Alexa.SkillBuilders.standard();
 
-// getRandomItem
-function getRandomItem(arrayOfItems) {
-  // the argument is an array [] of words or phrases
-  let i = 0;
-  i = Math.floor(Math.random() * arrayOfItems.length);
-  return (arrayOfItems[i]);
-};
-
-/* LAMBDA SETUP */
 exports.handler = skillBuilder
   .addRequestHandlers(
-    LaunchRequestHandler,
-    QuestionHandler,
+    GetNewFactHandler,
     HelpHandler,
-    RepeatHandler,
     ExitHandler,
     SessionEndedRequestHandler
   )
-  .addRequestInterceptors(LocalizationInterceptor)
   .addErrorHandlers(ErrorHandler)
   .lambda();
--- lambda/custom/questions.js
+++ lambda/custom/questions.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     'QUESTION_EN_US': {
-      'snow golem': 'A snow golem can be created by placing a pumpkin on top of  two snow blocks on the ground.',
+      'service dog': 'A snow golem can be created by placing a pumpkin on top of  two snow blocks on the ground.',
       'pillar quartz block': 'A pillar of quartz can be obtained by placing a block of quartz on top of a block of quartz in mine craft.',
       'firework rocket': 'A firework rocket can be crafted by placing a firework star in the left middle square, a piece of paper in the center square, and gunpowder in the right middle square in a crafting table. Similar to a firework star, a firework rocket can have more gunpowder added in the bottom row to increase the duration of a rocket.',
       'rabbit stew': 'Rabbit stew can be crafted by placing cooked rabbit in the top middle square, a carrot in the middle left square, a baked potato in the center square, any type of mushroom in the middle right square, and a bowl in the bottom middle square.',
@@ -12,7 +12,7 @@ module.exports = {
       'stone shovel': 'All types of shovels can be crafted by placing the desired material in the top middle square, and then sticks in the two squares directly beneath that in a crafting table.',
       'red carpet': 'Any type of carpet can be crafted by placing two wool, of the same color, next to each other in a crafting window.',
       'book and quill': 'A book and quill can be crafted by placing a book in the middle left square, an ink sac in the very center square, and a feather in the bottom middle square in a crafting table.',
-      'item frame': 'An item frame can be crafted by placing leather in the very center square, and eight sticks surrounding it.',
+      'animal frame': 'An animal frame can be crafted by placing leather in the very center square, and eight sticks surrounding it.',
       'map': 'A map can be crafted by placing a compass in the middle square and eight pieces of paper surrounding it.',
       'sticky piston': 'A sticky piston can be crafted by placing a slime ball on top of a piston in a crafting window',
       'bread': 'Bread can be crafted by placing three wheat across a row in a crafting table.',
